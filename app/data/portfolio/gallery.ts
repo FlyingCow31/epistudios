34946c15ff7dfa69e25954438bac109ffe1715b6
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-06-17 15:39:55 +0200

    feat: add more information for the gallery items

--- app/data/portfolio/gallery.ts
+++ app/data/portfolio/gallery.ts
@@ -1,42 +1,58 @@
 export const GalleryList = [
     {
         src: "/assets/galleries/toit-japonais.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/photographe-pote.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/flowers.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/moud.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/m.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/la-musique-note.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/volkswagen.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
     {
         src: "/assets/galleries/serax-fedrigoni.png",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
         credit: "Loan ? Ilan ?",
+        link: "https://www.google.com",
         alt: "dfjiodjifdojfikosdjfidojiof"
     },
 ]
